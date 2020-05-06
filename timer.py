3ccf16c7995ba86fe929232df444c72b8fab00db
TabenGH <tabentv@gmail.com>
2020-05-06 17:20:37 +0100

    Add timer

---
+++ timer.py
@@ -0,0 +1,23 @@
+import time
+
+class timer:
+
+    def __init__(self):
+        self.start = time.time()
+        print("init timer")
+
+    def start(self):
+        self.reset()
+
+    def reset(self):
+        self.start = time.time()
+
+    def getTime(self, isMilliseconds = True):
+        t = time.time() - start
+        if isMilliseconds:
+            return t * 1000
+        else:
+            return t
+
+    def print(self):
+        print(self.getTime())
