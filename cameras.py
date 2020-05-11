67edd70147bf501f54c1f950ccc36a54e1a69e1a
TabenGH <tabentv@gmail.com>
2020-05-11 14:08:00 +0100

    not much

--- cameras.py
+++ cameras.py
@@ -21,7 +21,7 @@ class camera:
     # Camera by default looks towards -z direction
 
     def __init__(self):
-        self.pos = maths3d.vec3(0, 64, 0)
+        self.pos = maths3d.vec3(0, 70, 0)
         self.vAngle = 0
         self.hAngle = 0
         self.timer = timer.timer()
--- minecraft.py
+++ minecraft.py
@@ -101,7 +101,6 @@ def main():
         shader.use()
 
         glUniformMatrix4fv(shader.locations[b"proj"], 1, uniform_transpose, maths3d.m4_projection(45, WINDOW_WIDTH/WINDOW_HEIGHT, 0.1, 1000).m)
-        view = camera.view
         glUniformMatrix4fv(shader.locations[b"view"], 1, uniform_transpose, camera.view.m)
         glUniformMatrix4fv(shader.locations[b"modelBlock"], 1, uniform_transpose, maths3d.mat4().m)
         glUniformMatrix4fv(shader.locations[b"modelChunk"], 1, uniform_transpose, maths3d.mat4().m)
