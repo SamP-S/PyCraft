b66bdb9718e181f80b7dfec5da2d3b1aa766a729
TabenGH <tabentv@gmail.com>
2020-05-12 21:55:57 +0100

    Transform component

---
+++ Object.py
@@ -0,0 +1,8 @@
+from maths3d import *
+
+class transform:
+
+    def __init__(self, pos=vec3(), rotation=vec3(), scale=vec3()):
+        self.pos = pos
+        self.rotation = rotation
+        self.scale = scale
