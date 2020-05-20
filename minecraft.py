7d7ef325996451e6cc4dd0cb4274a84d8cbe6f2b
TabenGH <tabentv@gmail.com>
2020-05-20 19:03:48 +0100

    Rename render_engine.py -> graphics_engine.py

--- minecraft.py
+++ minecraft.py
@@ -2,7 +2,7 @@
 import pygame
 from pygame.locals import *
 
-import input
+
 from timer import *
 import terrain_2 as terrain
 
@@ -35,7 +35,7 @@ def handleEvents():
 
 ################################################################################
 # Input Management
-
+import input
 global mouse
 global keyboard
 mouse = input.mouse()
@@ -44,10 +44,14 @@ keyboard = input.keyboard()
 
 ################################################################################
 # GLOBALS
-
+import scene
 global world
 world = scene.world()
 world.children.append(terrain.chunk())
+
+import graphics_engine
+global graphics
+graphics = graphics_engine.render_engine()
 
 
 ################################################################################
@@ -55,26 +59,22 @@ world.children.append(terrain.chunk())
 
 def main():
     print("PyCraft")
+
+    ###### MOVE PYGAME DISPLAY TO GRAPHICS
     pygame.init()
-    window = pygame.display
-    pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), DOUBLEBUF|OPENGL)
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), DOUBLEBUF|OPENGL)
     pygame.display.set_caption("Render Engine")
     pygame.mouse.set_visible(False)
     pygame.event.set_grab(True)
-    renderer = render_engine()
     frame = 0
-
-    # OpenGL Settings
 
     t = timer()
     while True:
         #print("frame")
 
         handleEvents()
-        scene.camera.process(keyboard, mouse)
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-
-        renderer.render(world)
+        world.camera.process(keyboard, mouse)
+        graphics.render(world)
 
         pygame.display.flip()
         frame += 1
